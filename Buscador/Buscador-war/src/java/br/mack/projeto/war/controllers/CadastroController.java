ffb5da74ce597eee13b643ad2b7ff3d79b2352fe
Felipe Rocha <felipegr6@gmail.com>
2015-05-03 21:03:59 -0300

    Inclusão de login e cadastro.

---
+++ Buscador/Buscador-war/src/java/br/mack/projeto/war/controllers/CadastroController.java
@@ -0,0 +1,49 @@
+package br.mack.projeto.war.controllers;
+
+import br.mack.projeto.ejb.entities.Usuario;
+import br.mack.projeto.ejb.facades.UsuarioFacadeLocal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class CadastroController extends AbstractController {
+    
+    UsuarioFacadeLocal usuarioFacade = lookupUsuarioFacadeLocal();
+    
+    @Override
+    public void execute() {
+        
+        String nome = getRequest().getParameter("nome");
+        String email = getRequest().getParameter("nome");
+        String senha = getRequest().getParameter("senha");
+        String confirmarSenha = getRequest().getParameter("confirmar_senha");
+        
+        Usuario usuario = new Usuario();
+        
+        usuario.setAtivo(1);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        usuario.setTokenFb("");
+        
+        usuarioFacade.create(usuario);
+        
+        setReturnPage("index.html");
+        
+    }
+    
+    private UsuarioFacadeLocal lookupUsuarioFacadeLocal() {
+        
+        try {
+            Context c = new InitialContext();
+            return (UsuarioFacadeLocal) c.lookup("java:global/Buscador/Buscador-ejb/UsuarioFacade!br.mack.projeto.ejb.facades.UsuarioFacadeLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+        
+    }
+    
+}
